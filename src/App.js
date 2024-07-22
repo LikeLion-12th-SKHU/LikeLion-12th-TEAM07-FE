dc2b6e851f32814335aae91014a554b4571da4f0
giwoong <dev.choi0409@gmail.com>
2024-07-22 23:44:33 +0900

    feat: .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import CreateGame from "./components/createGame";
-import Home from "./components/Home";
-import Lobby from "./components/Lobby.jsx";
-import Ranking from "./components/Ranking.jsx";
-import LoginPage from "./components/LoginPage.jsx";
+import CreateGame from "./components/createGame"; // 방 만들기
+import Home from "./components/Home"; // 홈
+import Lobby from "./components/Lobby"; // 로비
+import Ranking from "./components/Ranking"; // 랭킹
+import AutoAudio from "./components/AutoAudio"; // 배경음악
+import Setting from "./components/Setting"; // 설정
+import GameRoom from "./components/GameRoom"; // 게임 방
+import GameRoomList from "./components/GameRoomList"; // 게임 방 목록
+import LoginPage from "./components/LoginPage"; // 로그인 페이지
+import RoomSettings from "./components/RoomSettings"; // 방 설정
+
 function App() {
+  const [volume, setVolume] = useState(
+    parseFloat(localStorage.getItem("volume")) || 1
+  );
+  const [isSettingOpen, setIsSettingOpen] = useState(false);
+  const [rooms, setRooms] = useState([]);
+
+  const handleVolumeChange = (newVolume) => {
+    setVolume(newVolume);
+    localStorage.setItem("volume", newVolume);
+  };
+
+  const handleCreateGame = (newRoom) => {
+    setRooms((prevRooms) => [...prevRooms, newRoom]);
+    console.log("새로운 방이 생성:", newRoom);
+  };
+
+  const handleUpdateRoom = (updatedRoom) => {
+    setRooms((prevRooms) =>
+      prevRooms.map((room) => (room.id === updatedRoom.id ? updatedRoom : room))
+    );
+    console.log("업데이트 방 정보:", updatedRoom);
+  };
+
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<Lobby />} />
-        <Route path="/create-game" element={<CreateGame />} />
-        <Route path="/Home-go" element={<Home />} />
-        <Route path="/Ranking-go" element={<Ranking />} />
+        <Route
+          path="/"
+          element={<Lobby openSettings={() => setIsSettingOpen(true)} />}
+        />
+        <Route
+          path="/create-game"
+          element={
+            <CreateGame
+              onCreate={handleCreateGame}
+              openSettings={() => setIsSettingOpen(true)}
+            />
+          }
+        />
+        <Route
+          path="/Home-go"
+          element={<Home openSettings={() => setIsSettingOpen(true)} />}
+        />
+        <Route
+          path="/Ranking-go"
+          element={<Ranking openSettings={() => setIsSettingOpen(true)} />}
+        />
+        <Route
+          path="/room/:roomId"
+          element={<GameRoom openSettings={() => setIsSettingOpen(true)} />}
+        />
+        <Route path="/rooms" element={<GameRoomList rooms={rooms} />} />
+        <Route
+          path="/room-settings"
+          element={<RoomSettings onUpdate={handleUpdateRoom} />}
+        />
       </Routes>
+      <AutoAudio volume={volume} />
+      {isSettingOpen && (
+        <Setting
+          onClose={() => setIsSettingOpen(false)}
+          onVolumeChange={handleVolumeChange}
+        />
+      )}
     </Router>
   );
 }
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import {
   AppContainer,
   LoginContainer,
@@ -13,14 +15,73 @@ import KakaLoginLogo from "../assets/KakaoLoginLogo.png";
 import GuestLogin from "../assets/GuestLogin.png";
 
 export default function LoginPage() {
+  const [code, setCode] = useState(null);
+  const [idToken, setIdToken] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
+    const provider = localStorage.getItem("provider");
+
+    if (code) {
+      setCode(code);
+      getToken(code, provider);
+    }
+  }, [navigate]);
+
+  const kakaoHandleLogin = () => {
+    localStorage.setItem("provider", "kakao");
+    window.location.href =
+      "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=627aa5c7da11793c0aa31902e4af4051&redirect_uri=http://localhost:3000/login";
+  };
+
+  const googleHandleLogin = () => {
+    localStorage.setItem("provider", "google");
+    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=994954719923-uhpc7edlvk6cnfe25iscnmdsnlbhtpmr.apps.googleusercontent.com&redirect_uri=http://localhost:3000/login&response_type=code&scope=email profile`;
+  };
+
+  const getToken = async (authCode, provider) => {
+    try {
+      const idTokenResponse = await axios.get(
+        `http://localhost:8090/api/${provider}/id-token?code=${authCode}`
+      );
+      const idToken = idTokenResponse.data.data;
+      setIdToken(idToken);
+
+      const tokenResponse = await axios.post(
+        `http://localhost:8090/api/${provider}/token`,
+        {
+          authCode: idToken,
+        }
+      );
+
+      console.log(tokenResponse);
+
+      if (tokenResponse.data) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.error("Error fetching token:", error);
+    }
+  };
+
   return (
     <AppContainer>
       <LoginContainer>
         <LogoImage alt="Logo" src={Logo} />
         <LikeLiarImage alt="LikeLiar" src={LikeLiar} />
 
-        <LoginButton alt="카카오 로그인" src={KakaLoginLogo}></LoginButton>
-        <LoginButton alt="구글 로그인" src={GoogleLoginLogo}></LoginButton>
+        <LoginButton
+          alt="카카오 로그인"
+          src={KakaLoginLogo}
+          onClick={kakaoHandleLogin}
+        ></LoginButton>
+        <LoginButton
+          alt="구글 로그인"
+          src={GoogleLoginLogo}
+          onClick={googleHandleLogin}
+        ></LoginButton>
         <LoginButton alt="게스트 로그인" src={GuestLogin}></LoginButton>
       </LoginContainer>
     </AppContainer>
